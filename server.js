faf5b2e723fdcd40e06dfd6223c9ab1998ed03e0
Samuel Jacobs <sammyjacobscontact@gmail.com>
2024-01-04 15:57:31 +0000

    added validation and processing of user query

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import xssFilters from "xss-filters";
+import stopwords from "stopword";
 
 dotenv.config();
 
@@ -14,11 +16,42 @@ app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
 
+function validateQuery(query) {
+  if (query.length >= 140) {
+    throw new Error("Query too long");
+  }
+
+  if (typeof query !== "string") {
+    throw new Error("Query must be a string");
+  }
+
+  const sanitisedQuery = xssFilters.inHTMLData(query);
+
+  return sanitisedQuery;
+}
+
+function processQuery(query) {
+  const withoutPunctuation = query
+    .replace(/[^a-zA-Z0-9]/g, " ")
+    .replace(/\s+/g, " ")
+    .trim();
+
+  const words = withoutPunctuation.split(" ");
+  const withoutStopwords = stopwords.removeStopwords(words);
+  const withoutDuplicates = [...new Set(withoutStopwords)];
+  const processedQuery = withoutDuplicates.join(" ");
+
+  return processedQuery;
+}
+
 app.post("/search", (req, res) => {
   const userQuery = req.body.search.toLowerCase();
 
+  const validatedQuery = validateQuery(userQuery);
+  const processedData = processQuery(validatedQuery);
+
   res.send(`
-    <p class="px-4 py-2 bg-gray-800 text-white">${userQuery}</p>
+    <p class="px-4 py-2 bg-gray-800 text-white">${processedData}</p>
   `);
 });
 
